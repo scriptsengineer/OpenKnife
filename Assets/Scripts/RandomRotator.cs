45500b9486f0b90c3d1151fa87404bc01f8a561a
Rafael Correa <scriptsengineer@gmail.com>
2020-12-09 15:14:44 -0300

    Create Unity project

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(0,speed,0);
+    }
+}
